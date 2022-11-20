e2a6d7464f8b90c65a162463014f2505f67b8a77
Akim <akzar67@mail.ru>
2022-11-20 11:01:54 +0300

    added Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+int numberC = 12;
+int numberD = 5;
+Console.WriteLine(numberC / numberD);
+
+double numberE = 12;
+double numberF = 5;
+Console.WriteLine(numberE / numberF);
+
