978f12636d58615ec61d98bdf33937515579a532
Akim <akzar67@mail.ru>
2022-11-20 11:54:05 +0300

    добавил примеры с РАНДОМ

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -6,3 +6,16 @@ int numberD = 50;
 int result = numberC + numberD;
 Console.WriteLine(result);
 
+int numberE = new Random().Next(1, 10); //1 2 3 ...9
+int numberF = new Random().Next(1, 10);
+int result1 = numberE + numberF;
+Console.WriteLine(result1);
+
+int numberG = new Random().Next(1, 10); //1 2 3 ...9
+Console.WriteLine(numberG); // здесь добавил Console.WriteLine(numberG) и Console.WriteLine(numberH) чтобы 
+//видеть какие числа участвуют
+int numberH = new Random().Next(1, 10); //
+Console.WriteLine(numberH);
+int result2 = numberG + numberH;
+Console.WriteLine(result2);
+
